ec0d32342b0cac65daecc53bc1a235d8c72b21e1
zbw322092 <bz4e14@soton.ac.uk>
2016-06-20 18:34:38 +0800

    null undefined boolean

---
+++ null_undefined.js
@@ -0,0 +1,29 @@
+// undefined means a variable has been declared but has not yet been assigned a value.
+var a;
+console.log(a);
+console.log(typeof(a));
+
+var aa = undefined;
+console.log(aa);
+// console.log(a.toString());
+
+
+
+// null is an assignment value. It can be assigned to a variable as a representation of no value.
+var b = null;
+console.log(b);
+console.log(typeof(b));
+// console.log(b.toString());
+
+
+
+var c = [undefined, undefined, undefined];
+console.log(c.length);
+console.log(Boolean(c));
+
+console.log(Boolean([]));
+console.log(Boolean(""));
+console.log(Boolean(undefined));
+console.log(Boolean(null));
+
+
