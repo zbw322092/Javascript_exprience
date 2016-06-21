4a41014f08141a3fed62039cc23816c008ce2631
zbw322092 <bz4e14@soton.ac.uk>
2016-06-21 17:34:37 +0800

    operators priorty

--- assign_values.js
+++ assign_values.js
@@ -4,3 +4,12 @@ console.log("a: ", a , " b: ", b);
 var c = 2;
 var d = c = 3;
 console.log("c: ", c, " d: ", d);
+
+var e = 10, ee = 20;
+var f = e == 10;
+console.log("f: ", f);
+f = 100;
+console.log("f: ", f);
+
+var ff = e == 10 && ee == 10;
+console.log("ff: ", ff);
